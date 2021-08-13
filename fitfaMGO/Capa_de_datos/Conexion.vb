775d9abeaf9ae7e77a9c0171df26bc01f8bf801d
hamersoft <marianogo@hotmail.com>
2021-08-13 15:42:38 -0300

    choco 13-08-2021

--- fitfaMGO/Capa_de_datos/Conexion.vb
+++ fitfaMGO/Capa_de_datos/Conexion.vb
@@ -12,7 +12,7 @@ Public Class Conexion
     ' Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=fitfaBD;Data Source=(local)")
 
     ''local sin seguridad
-    Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Persist Security Info=False;User ID=choco; password=123choco;Initial Catalog=fitfaBD;Data Source=(local)")
+    Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Password=123choco;Persist Security Info=True;User ID=CHOCO;Initial Catalog=fitfaBD;Data Source=DESKTOP-IPJ62B9\SQLEXPRESS_CHOK")
 
     'hamer conexion
     ' Public dbconn As New OleDb.OleDbConnection("Provider=SQLOLEDB.1;Persist Security Info=False;User ID=choco; password=123choco;Initial Catalog=fitfaBD;Data Source=HAMER")
